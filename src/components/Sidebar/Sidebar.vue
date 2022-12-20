ca863a5a4185d3bec1fdf878c770d11ad25edbea
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-12-20 15:12:42 +0530

    Section 2 and 3 Toggle is working

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -970,7 +970,7 @@
         class="flex flex-col justify-start items-center px-6 border-b border-gray-600 w-full"
       >
         <button
-          onclick="showMenu2(true)"
+          @click="toggleVisible('SectionTwo')"
           class="focus:outline-none focus:text-indigo-400 text-white flex justify-between items-center w-full py-5 space-x-14"
         >
           <!-- <router-link>
@@ -998,7 +998,10 @@
             />
           </svg>
         </button>
-        <div class="flex justify-start flex-col items-start pb-5">
+        <div
+          class="flex justify-start flex-col items-start pb-5"
+          :class="{ hidden: this.isSectionTwoVisible }"
+        >
           <button
             class="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2 w-full md:w-52"
           >
@@ -1316,7 +1319,7 @@
       </div>
       <div class="flex flex-col items-center h-full pb-6 px-6 w-full">
         <button
-          onclick="showMenu3(true)"
+          @click="toggleVisible('SectionThree')"
           class="focus:outline-none focus:text-indigo-400 text-white flex justify-between items-center w-full py-5 space-x-14"
         >
           <p class="text-sm leading-5 uppercase">Section 3</p>
@@ -1341,6 +1344,7 @@
         <div
           id="Section 3"
           class="flex justify-start flex-col items-start pb-5"
+          :class="{ hidden: this.isSectionThreeVisible }"
         >
           <button
             class="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2 w-52"
