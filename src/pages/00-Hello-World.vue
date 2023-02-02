b41be9b4e5a03223ae89182d3ce4b23521eb3dc9
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-02-02 21:24:05 +0530

    components: {     HelloWorld,   },

--- src/pages/00-Hello-World.vue
+++ src/pages/00-Hello-World.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-    Hello World
     <HelloWorld />
   </div>
 </template>
 
 <script>
 import HelloWorld from "../components/00-Hello-World/HelloWorld.vue";
-export default {};
+export default {
+  components: {
+    HelloWorld,
+  },
+};
 </script>
 
 <style scoped></style>
